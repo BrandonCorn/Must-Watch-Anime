5fd1affc1175ed6cef8c80a67706e65df2249d71
BrandonCorn <corn_brandon@columbusstate.edu>
2020-07-14 00:42:35 -0400

    add link to page that will contain anime list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route, 
+  Link 
+} from 'react-router-dom'
 import './index.css';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Views/Home'
+import Anime from './Views/Anime'
 
 function App() {
   return (
     <div className = 'relative pb-10 min-h-screen bg-overlay bg-faded'>
-      <Header /> 
-      <Home /> 
-      <Footer /> 
+      <Router> 
+        <Header /> 
+        <div> 
+          <Switch> 
+
+            <Route exact path = '/'> 
+              <Home /> 
+            </Route> 
+
+            <Route path = '/anime'> 
+              <Anime /> 
+            </Route> 
+
+          </Switch> 
+        </div> 
+        <Footer /> 
+      </Router> 
     </div>
   );
 }
---
+++ src/Views/Anime.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Anime(props){
+    return (
+        <div className = 'flex justify-center animate-pop-in'> 
+            <ul> 
+                <li> 
+                    Anime 1
+                </li> 
+                <li> 
+                    Anime 2
+                </li> 
+            </ul> 
+        </div> 
+    )
+}
+
+export default Anime; 

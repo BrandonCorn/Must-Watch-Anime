7966eedabeb6ad8547c4ba1845959496fff95095
BrandonCorn <corn_brandon@columbusstate.edu>
2020-07-17 20:27:20 -0400

    add back button link to anime page list

--- src/Components/AnimeCard.js
+++ src/Components/AnimeCard.js
@@ -3,7 +3,7 @@ import { useAxiosGet } from '../Hooks/HttpRequest'
 import { Link } from 'react-router-dom'
 
 function AnimeCard(props) {
-    const [showAnime, setShowAnime] = useState(true); 
+    const [showAnimeCard, setShowAnimeCard] = useState(true); 
     const url = `https://5f10a1afd40d13001631d3a1.mockapi.io/api/v1/animes/${props.anime.id}`; 
     const animeDoc = useAxiosGet(url); 
     const domRef = useRef(); 
@@ -12,7 +12,7 @@ function AnimeCard(props) {
      
     useEffect( () => {
         const observer = new IntersectionObserver(entries => {
-            entries.forEach( entry => setShowAnime(entry.isIntersecting))
+            entries.forEach( entry => setShowAnimeCard(entry.isIntersecting))
         })
         observer.observe(domRef.current); 
         return () => observer.unobserve(domRef.current)  
@@ -21,7 +21,7 @@ function AnimeCard(props) {
 
     
     return (
-        <div ref = {domRef} className = {`animate-${showAnime ? 'slide-in' : ''}`}> 
+        <div ref = {domRef} className = {`animate-${showAnimeCard ? 'slide-in' : ''}`}> 
         <div className = 'flex justify-center mb-4'>
             <div className = 'w-1/2 m-4'>
                 
--- src/Views/AnimeProfile.js
+++ src/Views/AnimeProfile.js
@@ -1,73 +1,86 @@
 import React from 'react'
 
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function AnimeProfile(props){
     const location = useLocation(); 
     const anime = location.state.anime; 
     console.log(anime); 
     return (
-        <div className = 'flex m-10 animate-pop-in'> 
-            <div className = 'w-1/2'> 
-                <img src = {anime.image[1]} /> 
-                <div className = 'my-10'> 
-                    <ul> 
-                        <li className = 'table-row'> 
-                            <div className = 'table-cell w-1/3'>
-                            <span className = 'font-bold text-lg italic'> Release Date:  </span> 
-                            </div> 
-                            <div className = 'table-cell '> {anime.release_date} </div> 
-                            
-                        </li> 
-                        <li className = 'table-row'> 
-                            <div className = 'table-cell'> 
-                            <span className = 'font-bold text-lg italic'> Genres: </span> 
-                            </div> 
-                            <div className = 'table-cell'> 
-                            {anime.genres.map( genre => genre + " ")}
-                            </div> 
-                        </li>
-                        <li className = 'table-row'> 
-                            <div className = 'table-cell'> 
-                            <span className = 'font-bold text-lg italic'> Audio: </span> 
-                            </div> 
-                            <div className = 'table-cell'> 
-                            {anime.audio.map( audio => audio + " ")}
-                            </div> 
-                        </li> 
-                        <li className = 'table-row'> 
-                            <div className = 'table-cell'> 
-                            <span className = 'font-bold text-lg italic'> Subtitles: </span> 
-                            </div> 
-                            <div className = 'table-cell'> 
-                            {anime.subtitle.map( subtitle => subtitle + " ")}
-                            </div> 
-                        </li> 
-                    </ul> 
-                </div> 
-            </div> 
-            <div className='w-1/2'>
-                <div className='text-center mb-3'>
-                    <span className='text-3xl'>
-                        {anime.title}
-                    </span>
+        <div className='flex m-10 animate-pop-in'>
+
+                <div className = 'w-1/2'>
+                    <Link to = '/anime'> 
+                    <div className = 'my-6 border-4 w-1/5 text-center hover:bg-gray-700'>  
+                        <span className = 'text-xl'> Back </span> 
+                    </div>
+                    </Link>  
+                    <img src={anime.image[1]} />
+                    <div className='my-10'>
+                        <ul>
+                            <li className='table-row'>
+                                <div className='table-cell w-1/3'>
+                                    <span className='font-bold text-lg italic'> Release Date:  </span>
+                                </div>
+                                <div className='table-cell '> {anime.release_date} </div>
+
+                            </li>
+                            <li className='table-row'>
+                                <div className='table-cell'>
+                                    <span className='font-bold text-lg italic'> Genres: </span>
+                                </div>
+                                <div className='table-cell'>
+                                    {anime.genres.map(genre => genre + " ")}
+                                </div>
+                            </li>
+                            <li className='table-row'>
+                                <div className='table-cell'>
+                                    <span className='font-bold text-lg italic'> Audio: </span>
+                                </div>
+                                <div className='table-cell'>
+                                    {anime.audio.map(audio => audio + " ")}
+                                </div>
+                            </li>
+                            <li className='table-row'>
+                                <div className='table-cell'>
+                                    <span className='font-bold text-lg italic'> Subtitles: </span>
+                                </div>
+                                <div className='table-cell'>
+                                    {anime.subtitle.map(subtitle => subtitle + " ")}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <ul>
-                        <li>
-                            {anime.description[0]}
-                        </li>
-                        <li>
-                            {anime.description[1]}
-                        </li>
-                    </ul>
+                <div className = 'w-1/2'>
+                    <div className='text-center mb-3'>
+                        <span className='text-3xl'>
+                            {anime.title}
+                        </span>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                {anime.description[0]}
+                            </li>
+                            <li>
+                                {anime.description[1]}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='flex justify-center my-4 inline'>
+                        <span className=''>
+                            {anime.stream_services.map(service => service + "   ")}
+                        </span>
+                    </div>
                 </div>
-                <div className = 'flex justify-center my-4 inline'> 
-                    <span className = ''> 
-                        {anime.stream_services.map( service => service + "   ")}
-                    </span> 
+                
+            {/* <div className = 'table-row'> 
+                <div className = 'table-cell'> 
+                    <span> Brandon </span> 
                 </div> 
-            </div> 
+            </div>  */}
+            
         </div> 
     )
 }
